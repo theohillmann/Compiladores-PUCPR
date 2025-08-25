4d49e261cc030f41c94049e2ab3cbfd966c32925
theo hillmann Luz Coelho <70286183+theohillmann@users.noreply.github.com>
2025-08-24 22:17:00 -0300

    feat: refactor executar_expressao for improved operation handling and error management

--- RA1 - Analisador Lexico/executar_expressao.py
+++ RA1 - Analisador Lexico/executar_expressao.py
@@ -4,20 +4,14 @@ def executar_expressao(tokens: list, resultados: list, memorias: dict):
 
     while token_index < len(tokens):
         current_token = tokens[token_index]
-        print(current_token)
+        # print(current_token)
 
         if current_token in ["(", ")"]:
             token_index += 1
             continue
 
-        elif current_token in ["+", "-", "*", "/", "%", "^"]:
-            if len(stack_calculation) < 2:
-                return f"Erro: {current_token} requires two operands"
-
-            right_operator = stack_calculation.pop()
-            left_perator = stack_calculation.pop()
-
-            result = f"{right_operator} {current_token} {left_perator}"
+        elif current_token in ["+", "-", "*", "/", "%", "^", "^"]:
+            result = process_operation(stack_calculation, current_token)
 
         else:
             try:
@@ -27,7 +21,40 @@ def executar_expressao(tokens: list, resultados: list, memorias: dict):
                 return f"Erro: Invalid token '{current_token}'"
 
         token_index += 1
-    print(result)
+
+    return result
 
 
-print(executar_expressao(["(", "3.14", "2.0", "+", ")"], "", ""))
+def process_operation(stack_calculation, operator):
+    if len(stack_calculation) < 2:
+        return f"Erro: {operator} requires two operands"
+
+    right_operator = stack_calculation.pop()
+    left_perator = stack_calculation.pop()
+
+    return execute_operation(right_operator, operator, left_perator)
+
+
+def execute_operation(
+    right_operator: float, operator: str, left_operator: float
+) -> float:
+    try:
+        if operator == "^":
+            return left_operator**right_operator
+        return eval(f"{left_operator} {operator} {right_operator}")
+    except ZeroDivisionError:
+        return "Erro: Division by zero"
+    except Exception as e:
+        return "Error: operation failed"
+
+
+print(executar_expressao(["(", "2", "2.0", "+", ")"], "", ""))
+print(executar_expressao(["(", "3", "2.0", "*", ")"], "", ""))
+print(executar_expressao(["(", "10", "5", "/", ")"], "", ""))
+print(executar_expressao(["(", "2", "2", "-", ")"], "", ""))
+print(executar_expressao(["(", "11", "2", "%", ")"], "", ""))
+print(executar_expressao(["(", "2", "2", "^", ")"], "", ""))
+print(executar_expressao(["(", "2", "0", "%", ")"], "", ""))
+print(executar_expressao(["(", "2", "0", "/", ")"], "", ""))
+print(executar_expressao(["(", "2", "0", "$", ")"], "", ""))
+print(executar_expressao(["(", "2", "+", "0", ")"], "", ""))
